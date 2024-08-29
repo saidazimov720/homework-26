0dbb26e4f8e0ba0206812feacbaf14148886e481
saidazimov720 <saidazimovnurbek01@gmail.com>
2024-08-29 20:54:34 +0500

    links object

--- fetchData.js
+++ fetchData.js
@@ -2,5 +2,7 @@ const customEndPoints = {
     photos: " https://jsonplaceholder.typicode.com/photos",
     posts: "https://jsonplaceholder.typicode.com/posts",
     comments: "https://jsonplaceholder.typicode.com/comments",
-    
-}
+    albums: "https://jsonplaceholder.typicode.com/albums",
+    users: "https://jsonplaceholder.typicode.com/users",
+    todos: "https://jsonplaceholder.typicode.com/todos"
+};
