f480630420ccf1bd98d98f3baef18d3f5ebbf376
saidazimov720 <saidazimovnurbek01@gmail.com>
2024-08-29 21:21:35 +0500

    event listener is end

--- addNewItem.js
+++ addNewItem.js
@@ -1,4 +1,10 @@
 document.getElementById("newItemForm").addEventListener("submit", function(event)  {
     event.preventDefault();
     const newItemtype = document.getElementById("newItemType").value.trim().toLoweCase();
-})
+    const itemCount = parseInt(document.getElementById("itemCount").value, 10);
+    if (newItemtype && !isNaN(itemCount) && customEndPoints[newItemtype]) {
+        fetchAndDisplayNewItem(newItemtype, itemCount);
+    } else {
+        console.error("Invalid input");
+    }
+});
