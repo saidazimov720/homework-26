d2aef2817a67b80d15d722f9b347a5466ba3ac03
saidazimov720 <saidazimovnurbek01@gmail.com>
2024-08-29 20:53:14 +0500

    fetch data

---
+++ fetchData.js
@@ -0,0 +1,6 @@
+const customEndPoints = {
+    photos: " https://jsonplaceholder.typicode.com/photos",
+    posts: "https://jsonplaceholder.typicode.com/posts",
+    comments: "https://jsonplaceholder.typicode.com/comments",
+    
+}
