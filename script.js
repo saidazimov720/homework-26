bde027a302c7ce724b4229a30cd831a18130e973
saidazimov720 <saidazimovnurbek01@gmail.com>
2024-08-29 18:10:00 +0500

    data Form and switch start

--- script.js
+++ script.js
@@ -1,3 +1,20 @@
 document.getElementById("dataForm").addEventListener("submit", function (event){
-    
+    event.preventDefault();
+    const query = document.getElementById("query").value;
+    const queries = query.split(',').map(item => item.trim());
+
+    queries.forEach(queryItem => {
+        const[key, value] = queryItem.split('=');
+        const count = parseInt(value, 10);
+        if (key && !isNaN(count)) {
+            switch (key.toLowerCase()) {
+                case 'photos':
+                    fetchPhotos(count);
+                    break;
+            
+                default:
+                    break;
+            }
+        }
+    });
 })
